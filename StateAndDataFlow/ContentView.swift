edc64ade7e5034b948b4e878085cb0f88151916e
Антоненко Александр <aalexanto@gmail.com>
2021-08-03 14:42:50 +0300

    Initial Commit

---
+++ StateAndDataFlow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StateAndDataFlow
+//
+//  Created by Alexander Antonenko on 03.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Alexander Antonenko on 03.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
